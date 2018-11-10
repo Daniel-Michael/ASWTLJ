417f051ba7972dcbdbdff80caa5acb8b1b092642
Dan Michael <danmikhael@gmail.com>
2018-11-10 20:02:44 +1100

    adding next lessons
    Starting at Lesson 22

---
+++ jQuery/lessons-3.js
@@ -0,0 +1,5 @@
+// Carrying on with lessons and exercises from A Smarter Way To Learn jQuery
+
+// Assign an element to a var then manipulate (change class) using the var
+var p1 = $("p#main");
+p1.attr("id", "secondary");
