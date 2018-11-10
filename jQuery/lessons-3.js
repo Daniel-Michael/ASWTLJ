d453d0b6b5672a962ac3873236d1d23811afb1dc
Dan Michael <danmikhael@gmail.com>
2018-11-10 20:05:35 +1100

    add exercise
    from lessson 22

--- jQuery/lessons-3.js
+++ jQuery/lessons-3.js
@@ -3,3 +3,8 @@
 // Assign an element to a var then manipulate (change class) using the var
 var p1 = $("p#main");
 p1.attr("id", "secondary");
+
+// assign an image to a var, then use var to manipulate (slideDown)
+
+var pic = $("img#dong");
+pic.slideDown();
